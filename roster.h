962b370f1fee74aeebef871b407835fe83c164fe
jlonge4 <91354480+jlonge4@users.noreply.github.com>
2021-11-24 18:35:01 -0500

    final code

--- roster.h
+++ roster.h
@@ -7,8 +7,26 @@ private:
 	const static int numStud = 5;
 	student* studentRosterArr[numStud];
 
+public:
 
+	void parse(string row);
 
+	void add(string sID,
+		string slastName,
+		string sfirstName,
+		string semail,
+		int sage,
+		int sdaysInCourse1,
+		int sdaysInCourse2,
+		int sdaysInCourse3,
+		DegreeProgram dP);
+	
+	void printAll();
+	void printByDegreeProgram(DegreeProgram dP);
+	void printInvalidSIDs();
+	void printAverageDaysInCourse();
+	void removeStudID(string studID);
+	~Repo();
 };
 
 
