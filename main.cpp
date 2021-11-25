906a4fabfeb9111c75cd60360b8a3de6398a0b71
jlonge4 <91354480+jlonge4@users.noreply.github.com>
2021-11-25 18:26:23 -0500

    final code, now debug it all

--- main.cpp
+++ main.cpp
@@ -9,5 +9,37 @@ int main()
         "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
         "A5,[firstname],[lastname],[emailaddress],[age], 
-        [numberofdaystocomplete3courses],SOFTWARE"}
+        [numberofdaystocomplete3courses],SOFTWARE"};
+    
+    const int numStud = 5;
+    Roster roster;
+
+    for (int i = 0; i < numStud; i++) roster.parse(studentData[i]);
+    cout << "Displaying all students: " std::endl;
+    roster.printAll();
+    cout << std::endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Displaying by Degree Program: " << degreeProgram[i] << std::endl;
+        roster.printByDegreeProgram((DegreeProgram)i);
+    }
+
+    cout << "Displaying students with invalid IDs" << std::endl;
+    roster.printInvalidSIDs();
+    cout << std::endl;
+
+    cout << "Displaying days in course: " << std::endl;
+    roster.printAverageDaysInCourse();
+
+    cout << "Removing student with ID " << std::endl;
+    roster.removeStudID("blah");
+    cout << std::endl;
+
+    cout << "Removing student with ID " << std::endl;
+    roster.removeStudID("blah");
+    cout << std::endl;
+
+    system("pause");
+    return 0;
 }
